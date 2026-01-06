f25a8f61758bc6d9b261fa36abae828a6dba10b6
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-06 23:17:47 +0400

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-"""
-Module containing function from numpy
-"""
 import pandas as pd
+
+
 def from_numpy(array):
     """
-        Creates a pd.DataFrame from a np.ndarray
-        The columns are labeled in alphabetical order and capitalized
-        """
-    # Get the number of columns in the array
-    cols = array.shape[1]
-
-    # Generate labels A, B, C... using ASCII values (65 is 'A')
-    col_names = [chr(65 + i) for i in range(cols)]
-
-    # Create the DataFrame using the provided array and generated names
-    return pd.DataFrame(array, columns=col_names)
+    Converting a numpy.ndarray into a pandas.DataFrame
+    """
+    # Your code here
+    return df
