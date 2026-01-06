84adc14a1100e6a2e6ceca06223f754bef502f01
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-07 00:09:55 +0400

    Pandas task

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -3,6 +3,4 @@
 TASK 1
 """
 import pandas as pd
-df = pd.DataFrame({ "First": [0.0, 0.5, 1.0, 1.5],
-      "Second": ["one", "two", "three", "four"]
-      }, index = ['A', 'B', 'C', 'D'])
+df = pd.DataFrame({"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}, index = ['A', 'B', 'C', 'D'])
