fbf84c18d61e6332485751994b1f93b920ab16a9
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-06 23:49:40 +0400

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -5,6 +5,8 @@ def from_numpy(array):
 """
 Fuction creating df from np.ndarray
 """
+    pd.set_option("display.expand_frame_repr", True)
+    pd.set_option("display.max_columns", None)
     df = pd.DataFrame(array)
-    df.columns = [chr(65+i) for i in range(df.shape[1])]
+    df.columns = [chr(65 + i) for i in range(array.shape[1])]
     return df
