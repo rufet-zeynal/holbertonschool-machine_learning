52fddd3d11c77bf98d0e643c3bdfc95f4eecc038
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-07 15:43:02 +0400

    Pandas task

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -9,6 +9,6 @@ def rename(df):
     """
     Rename columns of pandas dataframe and converting timestamp values to datetime format
     """
-    df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
+    df.rename(columns={"Timestamp": "Datetime"}, inplace=True)
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
-    return df[['Datetime', 'Close']]
+    return df[["Datetime", "Close"]]
