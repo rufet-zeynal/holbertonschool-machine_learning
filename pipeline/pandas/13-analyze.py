6a1bcebcb08c937ab081649f6f1e8d92f8ded3b5
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-07 21:57:02 +0400

    Pandas task

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -8,6 +8,5 @@ def analyze(df):
     """
     Description of columns except Timestamp
     """
-    df = df.describe
-    df = df.drop(columns=['Timestamp'])
+    df = df.drop(columns=['Timestamp']).describe
     return df
