77a2d906288ce77431ecc396c656e681af276215
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-07 21:49:04 +0400

    Pandas task

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -15,7 +15,7 @@ def hierarchy(df1, df2):
     initial, last = 1417411980, 1417417980
     df1_new = df1.loc[initial:last]
     df2_new = df2.loc[initial:last]
-    df = pd.concat([df2_new, df1_new], keys=['bitstamp', 'coinbase'])
+    df = pd.concat([df2_new, df1_new], keys = ['bitstamp', 'coinbase'])
     df = df.swaplevel(0,1)
     df = df.sort_index()
     return df
