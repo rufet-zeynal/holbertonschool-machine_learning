2c5f4d524bd83eb7641a31117a6d4f1c98502f3b
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-08 17:22:30 +0400

    Plotting

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -11,7 +11,7 @@ def line():
     Line graph plotting
     """
     y = np.arange(0, 11) ** 3
-    plt.figure(figsize=(6.4, 4.8)
+    plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, 'r-')
     plt.xlim(0, 10)
     plt.show()
