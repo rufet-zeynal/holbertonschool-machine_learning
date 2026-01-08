1268010dfb186ef268ebedd262488fbb8307fa4f
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-08 17:52:08 +0400

    Plotting

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Python plotting
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """
+    Change the scale of the figure
+    :return:
+    """
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
+    ax.set_yscale('log')
+    ax.grid(True)
+    plt.show()
