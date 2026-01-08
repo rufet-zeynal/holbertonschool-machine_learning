89e0ca0633a1cf0ea6e0dec5bf8cc39aa4162a40
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-08 18:25:53 +0400

    Plotting

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -15,6 +15,6 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
-    plt.hist(student_grades, bins=10)
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
     plt.title("Project A")
     plt.show()
