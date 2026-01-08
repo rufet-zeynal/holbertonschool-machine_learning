37fb8b534ff34a39f1471f56597d8deb40306e35
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-08 18:21:13 +0400

    Plotting

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Python plotting
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Histogram plot
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.hist(student_grades, bins=10)
+    plt.title("Project A")
+    plt.show()
