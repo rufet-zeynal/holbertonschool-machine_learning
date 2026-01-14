f2f5e10dd9994365a25da12a4afa48f0548d036c
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-14 14:44:09 +0400

    math

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,10 +2,7 @@
 
 
 def summation_i_squared(n):
-    total = 0
-    for i in range(1, n+1):
-        total += i**2
-    return total
+    return (n*(n+1)*(2*n+1))/6
 
 n = int(input("n = "))
 print(summation_i_squared(n))
