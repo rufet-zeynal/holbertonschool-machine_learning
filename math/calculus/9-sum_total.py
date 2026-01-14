2be101efb7210233cb953dada2684466593a2a9b
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-14 14:57:26 +0400

    math

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-
+"""
+Math task
+"""
 
 def summation_i_squared(n):
+    """
+    Sum of square numbers
+    """
     if type(n) is not int or n < 1:
         return None
     return n*(n+1)*(2*n+1)/6
