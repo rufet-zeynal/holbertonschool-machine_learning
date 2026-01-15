75f5174408bea5b5783673bd460034b369b27989
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-15 15:54:57 +0400

    math

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Math task
+"""
+
+
+def poly_derivative(poly):
+    """
+    Calculating the derivative of polynomial
+    """
+    if (type(poly) is not list or len(poly) == 0 or
+        not all(isinstance(i, int) for i in poly)):
+        return None
+    if len(poly) == 1:
+        return [0]
+    return [i * poly[i] for i in range(1, len(poly))]
+
+
+
