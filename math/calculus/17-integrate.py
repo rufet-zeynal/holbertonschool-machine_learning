7ed29e6e7b4a632263aac97640d638cfec9f7269
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-17 15:54:57 +0400

    math

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -10,9 +10,16 @@ def poly_integral(poly, C=0):
     """
     if (type(poly) is not list or len(poly) == 0
             or not all(isinstance(i, (int, float)) for i in poly)
-            or not isinstance(C, (int, float))):
+            or not isinstance(C, int)):
         return None
-    if len(poly) == 0:
-        return None
+
     integral = [C] + [poly[i] / (i+1) for i in range(len(poly))]
+    # Convert whole floats to int
+    integral = [int(x) if isinstance(x, float) and x.is_integer() else x
+                for x in integral]
+
+    # Remove trailing zeros
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
+
     return integral
