4ea71d513ff1cb71b2526fe207d3de85c59ab41a
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 08:44:09 +0000

    linear algebra

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Line Up
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Adding two arrays element-wise
+    """
+    if len(arr1) != len(arr2):
+        return None
+
+    return [arr1[i] arr2[i] for i in range(len(arr1))]
