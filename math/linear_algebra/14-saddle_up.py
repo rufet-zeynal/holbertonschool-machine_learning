457d6620c174607a12581f0c23816d8094ef5149
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 22:10:43 +0400

    linear algebra

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
