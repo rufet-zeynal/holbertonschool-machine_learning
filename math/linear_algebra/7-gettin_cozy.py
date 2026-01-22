1ac5f20cfd1e2504ae71338a621386ae4921e1e3
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 11:11:39 +0000

    linear algebra

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,11 +8,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Concatenating two matrices along a specific axis
     """
-
-     if (len(mat1[0]) == len(mat2[0])) and (axis == 0):
-             return (mat1 + mat2)
-
+    if (len(mat1[0]) == len(mat2[0])) and (axis == 0):
+        concat = mat1 + mat2
+        return concat
     elif (len(mat1) == len(mat2)) and (axis == 1):
-        return [mat1[i] + mat2[i] for i in range(len(mat1)])
+        concat = [mat1[j] + mat2[j] for j in range(len(mat1))]
+        return concat
     else:
         return None
