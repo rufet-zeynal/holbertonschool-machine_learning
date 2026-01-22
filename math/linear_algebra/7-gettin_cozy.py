1800b7bd9969e650ff62deb0f5f1f05759278909
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 11:07:03 +0000

    linear algebra

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,7 +10,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
 
      if (len(mat1[0]) == len(mat2[0])) and (axis == 0):
-             return mat1 + mat2
+             return (mat1 + mat2)
 
     elif (len(mat1) == len(mat2)) and (axis == 1):
         return [mat1[i] + mat2[i] for i in range(len(mat1)]
