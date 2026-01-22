f4f219b1ff763e68cc5a97820fe96b194499e8eb
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 21:59:58 +0400

    linear algebra

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Element operation
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Element-wise addition, subtraction, multiplication, and division
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
