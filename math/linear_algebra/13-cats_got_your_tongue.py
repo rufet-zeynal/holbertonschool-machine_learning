87b0541ac02cf47aa4f2f2fbb80e4e23b25b683b
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 22:03:51 +0400

    linear algebra

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Concatenating two matrices
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0)
+    """
+    Concatenating two matrices along a specific axis
+    """
+    return np.hstack((mat1, mat2))
