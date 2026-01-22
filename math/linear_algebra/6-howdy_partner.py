71b99b73fed085c42e702487ad1763335ead4fc9
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 10:00:45 +0000

    linear algebra

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -2,11 +2,10 @@
 """
 Howdy Partner
 """
-import numoy as np
 
 
 def cat_arrays(arr1, arr2):
     """
     Concatenating two arrays
     """
-    return np.concatenate(arr1, arr2)
+    return arr1 + arr2
