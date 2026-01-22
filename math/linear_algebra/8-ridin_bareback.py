f2c2e7e8dd7259ab179404e4d1df962a7dc946e3
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 12:28:56 +0000

    linear algebra

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -8,16 +8,18 @@ def mat_mul(mat1, mat2):
     """
     Matrix multiplication
     """
-    if len(mat1[0]) == len(mat2):
-            new_matrice =[]
-            for row1 in range(len(mat1)):
-                m = []
-                for col2 in range(len(mat2[0])):
-                    value = 0
-                    for col1 in range(len(mat1)):
-                        value += (mat1[row1][col1] * mat2[row1][col2])
-                    m.append(value)
-                new_matrice.append(m)
-            return new_matrice
-    else:
+    if len(mat1[0]) != len(mat2):
         return None
+        
+    new_matrice =[]
+    for row1 in range(len(mat1)):
+         m = []
+         for col2 in range(len(mat2[0])):
+            value = 0
+            for col1 in range(len(mat1)):
+                value += (mat1[row1][col1] * mat2[row1][col2])
+            m.append(value)
+         new_matrice.append(m)
+     return new_matrice
+else:
+    return None
