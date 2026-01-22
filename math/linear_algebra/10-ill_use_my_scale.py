27521b48848b3bfb024de2180857e36e5771cd93
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 21:51:47 +0400

    linear algebra

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Shape of ndarray
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Calculating the shape of a numpy.ndarray
+    """
+    return matrix.shape
