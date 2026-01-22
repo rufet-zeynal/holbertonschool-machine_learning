049334b35ac4b3a8df7bb5f224557064b7fdc17d
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-22 08:27:16 +0000

    linear algebra

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -9,7 +9,6 @@ def matrix_transpose(matrix):
     """
     if not matrix:
         return []
-    for row in matrix:
-        if not all(len(row) == len(matrix[0])):
-            return None
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        return None
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
