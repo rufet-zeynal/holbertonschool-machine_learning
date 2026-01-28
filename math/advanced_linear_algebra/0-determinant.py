ea435b3ea7a78711df16da399912b224755f1d95
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-28 21:45:01 +0400

    advanced linear algebra

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Determinant of a matrix
+"""
+
+
+def determinant(mat):
+    """
+    Calculation of determinant of a matrix
+    """
+    if not isinstance(mat, list) or any(not isinstance(row, list) for row in mat):
+        raise TypeError("matrix must be a list of lists")
+    if mat == [[]]
+        return 1
+    x = len(mat)
+    if any(len(row) != x for row in mat):
+        raise TypeError("matrix must be a square matrix")
+    if n == 1:
+        return mat[0][0]
+    if n == 2:
+        return mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
+
+    return sum((-1)**k * mat[0][k] * determinant([row[:k] + row[k+1:] for row in mat[1:]] for k in range(x)))
