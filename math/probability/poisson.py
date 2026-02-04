09c48606068e9f5e454f9845f97be8745c8ed208
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-04 22:16:14 +0400

    probability

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -19,7 +19,7 @@ class Poisson:
 
         else:
             if not isinstance(data, list):
-                raise ValueError("data must be a list")
+                raise TypeError("data must be a list")
 
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
