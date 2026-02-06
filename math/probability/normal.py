92324c04e3bd50203e9a3df350791dbb069cad12
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-06 14:47:50 +0400

    probability

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -51,23 +51,33 @@ class Normal:
         b = pow(e, (-(x - self.mean) ** 2) / (2 * (self.stddev ** 2)))
         return a * b
 
+    def factorial(self, x):
+        """
+        factorial function
+        """
+        if x == 0:
+            return 1
+
+        factorial = 1
+        for i in range(1, x + 1):
+            factorial *= i
+        return factorial
+
+    def erf(self, x):
+        """
+        erf function
+        """
+        pi = 3.1415926536
+        summation = sum((pow(-1, x) * pow(x, (2 * n + 1)) / (self.factorial(n) *
+                    (2 * n + 1)) for n in range(5)))
+        erf = (2 / (pi ** 0.5)) * summation
+        return erf
+
+
     def cdf(self, x):
         """
         cdf function
         """
-        e = 2.7182818285
-        y = (x - self.mean) / (self.stddev * (2 ** 0.5))
-
-        t = 1 / (1 + 0.3275911 * abs(y))
-        erf = 1 - (
-                0.254829592 * t -
-                0.284496736 * t ** 2 +
-                1.421413741 * t ** 3 -
-                1.453152027 * t ** 4 +
-                1.061405429 * t ** 5
-        ) * pow(e, -(y ** 2))
-
-        if y < 0:
-            erf = -erf
-
-        return 0.5 * (1 + erf)
+        erf = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        cdf = 0.5 * (1 + (self.erf(erf)))
+        return cdf
