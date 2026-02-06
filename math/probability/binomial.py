49c945b4191a3c59dca10af38b90771e93227d22
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-06 15:33:18 +0400

    probability

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -15,7 +15,7 @@ class Binomial:
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
-            if not (0 <= p <= 1):
+            if not (0 < p <= 1):
                 raise ValueError("p must be greater than 0 and less than 1")
             self.n = int(n)
             self.p = float(p)
@@ -29,8 +29,8 @@ class Binomial:
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / len(data)
 
-        p = 1 - (variance / mean)
-        n = round(mean / p)
-        p = mean / n
-        self.n = n
-        self.p = p
+            p = 1 - (variance / mean)
+            n = round(mean / p)
+            p = mean / n
+            self.n = n
+            self.p = p
