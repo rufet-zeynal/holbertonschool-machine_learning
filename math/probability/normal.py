52a8035e212050d6fb28a54312e2af6413d0abeb
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-06 13:16:28 +0400

    probability

---
+++ math/probability/normal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Normal distribution
+"""
+
+
+class Normal:
+    """
+    Normal distribution class
+    """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Initialization function
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = sum(data) / len(data)
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.stddev = variance ** 0.5
