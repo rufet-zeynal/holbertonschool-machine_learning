c16a6fe402274726b86ceace253b4a60f07b100d
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-08 14:13:56 +0400

    Bayesian Theorem

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -35,7 +35,7 @@ def intersection(x, n, P, Pr):
         raise TypeError(
             "Pr must be a numpy.ndarray with the same shape as P")
     if np.any((P < 0) | (P > 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError("All values in {P} must be in the range [0, 1]")
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
