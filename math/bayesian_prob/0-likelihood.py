8b13c0e4fd24876e6f692be1c8967523e0bc3567
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-08 13:34:21 +0400

    Bayesian Theorem

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Bayesian Probability Likelihood
+"""
+import numpy as np
+from math import comb
+
+def likelihood(x, n, P):
+    """
+    Likelihood function for Bayesian Probability Likelihood
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x <= 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+
+    if x > n :
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any(P < 0 or P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    c = comb(n, x)
+    return c * (P ** x) * ((1 - P) ** (n - x))
