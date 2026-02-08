a1aceec303ded4fb5968cca1b40033736480288d
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-02-08 13:43:47 +0400

    Bayesian Theorem

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -5,7 +5,7 @@ Bayesian Probability Likelihood
 import numpy as np
 
 
-def factorial(self, x):
+def factorial(x):
     """
     factorial function
     """
@@ -17,21 +17,22 @@ def factorial(self, x):
         fact *= i
     return fact
 
+
 def likelihood(x, n, P):
     """
     Likelihood function for Bayesian Probability Likelihood
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x <= 0:
+    if not isinstance(x, int) or x < 0:
         raise ValueError("x must be an integer that is greater than or equal to 0")
 
-    if x > n :
+    if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
 
-    if np.any(P < 0 or P > 1):
+    if np.any((P < 0) | (P > 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     c = factorial(n) / (factorial(n - x) * factorial(x))
