85c351ef39b3e54193d3b998cc214273d37c3a18
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-06 19:36:36 +0400

    Classification Using Neural Networks

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module for a single neuron performing binary classification
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Neuron class
+    """
+    def __init__(self, nx):
+        """
+        Class constructor for the Neuron.
+        nx (int): The number of input features to the neuron.
+        """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
