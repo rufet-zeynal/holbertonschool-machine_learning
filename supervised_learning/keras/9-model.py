6a018b632e49589394c6bfb9fb2da7e27a1c8bc9
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-12 19:22:23 +0400

    Deep Learning Keras

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Saves and loads Keras models
+"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    Saving an entire model to file
+    network  - the Keras model to save
+    filename - path to save the model to
+    """
+    network.save(filename)
+
+def load_model(filename):
+    """
+    Loading an entire model from file
+    """
+    return K.models.load_model(filename)
