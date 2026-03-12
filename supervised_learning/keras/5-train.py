bddbfc57aa7c5946f65648a97bf1bcaa31d17a38
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-12 18:21:03 +0400

    Deep Learning Keras

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""Training a model using mini-batch gradient descent"""
+"""
+Training a model using mini-batch gradient descent
+with validation
+"""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-        validation_data=None, verbose=True, shuffle=False):
+                validation_data=None, verbose=True, shuffle=False):
     """
     Training a model using mini-batch gradient descent
     network    - the Keras model to train
