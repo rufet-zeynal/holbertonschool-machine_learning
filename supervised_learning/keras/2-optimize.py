503512c810e7b406b4fbae3bfe04769c515320c1
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-12 17:50:23 +0400

    Deep Learning Keras

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -16,9 +16,10 @@ def optimize_model(network, alpha, beta1, beta2):
     """
     network.compile(
         optimizer=K.optimizers.Adam(
-            lr=alpha,
-            beta1=beta1,
-            beta2=beta2),
+            learning_rate=alpha,
+            beta_1=beta1,
+            beta_2=beta2
+        ),
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
