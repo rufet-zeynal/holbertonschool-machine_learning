d9c13327bed3beea9d28a841cf26998b26429203
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-14 21:47:03 +0400

    Deep Learning - Optimization

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+GD with momentum
+"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updating a variable using GD with momentum
+    alpha: learning rate
+    beta1: momentum weight
+    var: numpy.ndarray, variable to be updated
+    grad: numpy.ndarray, gradient of var
+    v: previous first moment of var
+    """
+    v_update = beta1 * v + (1 - beta1) * grad
+    var_update = beta1 * var + (1 - beta1) * grad
+
+    return v_update, var_update
