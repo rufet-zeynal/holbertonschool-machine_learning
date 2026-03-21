0dc0318d38bcf9bbd566f1cf59a04210bdbdd8bb
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-21 20:58:13 +0400

    Deep Learning - Regularization

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""L2 Regularization Cost"""
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+    Calculating the regularization cost
+    cost - cost without regularization
+    lambtha - regularization parameter
+    weights - dictionary of weights and biases
+    L - number of layers
+    m - number of nodes
+    """
+    l2_sum = 0
+    for i in range(1, L + 1):
+        w = weights['w' + str(i)]
+        l2_sum += np.sum(np.square(w))
+
+    l2_cost = (lambtha / (2 * m )) * l2_sum
+
+    return cost + l2_cost
---
+++ supervised_learning/regularization/README.md
@@ -0,0 +1,1 @@
+Regularization techniques of neural network
