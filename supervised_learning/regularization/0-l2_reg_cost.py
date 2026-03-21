39e46dcc2bf0fc7bfda1d24040135ab8b2da5a32
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-21 21:02:39 +0400

    Deep Learning - Regularization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""L2 Regularization Cost"""
+"""
+L2 Regularization Cost
+"""
 import numpy as np
 
 
@@ -15,7 +17,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     l2_sum = 0
     for i in range(1, L + 1):
         W = weights['W' + str(i)]
-        l2_sum += np.sum(np.square(w))
+        l2_sum += np.sum(np.square(W))
 
     l2_cost = (lambtha / (2 * m )) * l2_sum
 
