fea5233e8420008fb13f1c116f75f99824a50706
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-21 21:47:01 +0400

    Deep Learning - Regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -12,6 +12,6 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     L2 = tf.keras.regularizers.L2(lambtha)
     init = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
-    dense = tf.layers.Dense(
+    dense = tf.keras.layers.Dense(
         n, activation, kernel_regularizer=L2, kernel_initializer=init)
     return dense(prev)
