9b2574b7e4fd613b226c604358954ff528ab43a7
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-21 21:45:58 +0400

    Deep Learning - Regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -10,10 +10,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     activation: activation function
     lambtha:    L2 regularization parameter
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    L2 = tf.contrib.layers.l2_regularizer(lambtha)
-    # implements the operation: outputs = activation(inputs * kernel + bias)
-    #                                                         weigth
-    output = tf.layers.Dense(n, activation, kernel_initializer=init,
-                             kernel_regularizer=L2)
-    return output(prev)
+    L2 = tf.keras.regularizers.L2(lambtha)
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    dense = tf.layers.Dense(
+        n, activation, kernel_regularizer=L2, kernel_initializer=init)
+    return dense(prev)
