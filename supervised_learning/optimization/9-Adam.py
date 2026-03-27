32114948196c849fdce2b02288f47aa895dc7c8a
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-27 16:03:09 +0400

    Deep Learning - Optimization

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -3,8 +3,7 @@
 import numpy as np
 
 
-def update_variables_Adam(alpha, beta1, beta2, epsilon
-                          , var, grad, v, s, t):
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """
     Updates a variable using Adam optimization
 
