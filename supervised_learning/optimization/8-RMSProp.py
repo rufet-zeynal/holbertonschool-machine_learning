cdfa5b3b6da3c87c09fc0811a8ff5deeae40af9d
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-03-27 15:53:06 +0400

    Deep Learning - Optimization

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Task 8: RMSProp Upgraded
+Sets up the RMSProp optimization algorithm using TensorFlow.
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Sets up the RMSProp optimization algorithm in TensorFlow.
+
+    Args:
+        alpha   (float): Learning rate.
+        beta2   (float): RMSProp discounting factor (decay for squared-grad avg).
+        epsilon (float): Small constant to avoid division by zero.
+
+    Returns:
+        optimizer: A tf.keras.optimizers.RMSprop instance.
+    """
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,          # rho is TF's name for beta2 / discount factor
+        epsilon=epsilon
+    )
+    return optimizer
