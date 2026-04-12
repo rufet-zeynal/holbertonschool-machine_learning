8120d26ef0cbb55f75ad5e0bc54974728ca317f0
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-04-12 18:36:07 +0400

    Data Augmentation

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -9,6 +9,5 @@ def change_brightness(image, max_delta):
     """
     Brightness augmentation
     """
-    brightened_image = tf.image.random_brightness(image,
-                    max_delta)
+    brightened_image = tf.image.random_brightness(image, max_delta)
     return brightened_image
