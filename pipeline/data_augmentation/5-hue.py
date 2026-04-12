62391c52d622bfc692c4c42486e1ac04ec14b6d3
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-04-12 17:50:55 +0400

    Data Augmentation

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -9,6 +9,4 @@ def change_hue(image, delta):
     """
     Hue Changing of an image
     """
-    hue_adjusted_image = tf.image.adjust_hue(image,
-                        delta=0.1)
-    return hue_adjusted_image
+    return tf.image.random_hue(image, delta)
