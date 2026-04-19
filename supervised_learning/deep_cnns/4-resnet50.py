35386a6dfd3aac6c3ff4461be4fce6f0c89b662b
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-04-19 17:31:55 +0400

    Deep Convolutional Architectures

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""ResNet-50 architecture"""
+"""
+ResNet-50 architecture
+"""
 from tensorflow import keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
