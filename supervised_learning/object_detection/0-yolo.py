131387005c7cd14981e8bbb33ce17015609ce696
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-05-03 13:31:05 +0400

    Object Detection

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 0 - Initialize Yolo"""
+import tensorflow as tf
+import numpy as np
+
+
+class Yolo:
+    """Yolo v3 object detection"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = tf.keras.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f.readlines()]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
