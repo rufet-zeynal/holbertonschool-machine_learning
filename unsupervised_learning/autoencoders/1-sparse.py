12d09ce5e6721bb5e9bd1cd512457c03a6a22b8e
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-06-07 12:28:28 +0400

    Autoencoders

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -16,8 +16,11 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
         x = keras.layers.Dense(nodes, activation='relu')(x)
 
     reg = keras.regularizers.l1(lambtha)
-    latent_space = keras.layers.Dense(latent_dims,
-                    activation='relu', activity_regularizer=reg)(x)
+    latent_space = keras.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=reg
+    )(x)
 
     encoder = keras.Model(encoder_inputs, latent_space, name='encoder')
 
