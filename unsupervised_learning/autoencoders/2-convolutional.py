c30937aa1e5f94ba98f92770143184f972abb2d9
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-06-07 12:30:09 +0400

    Autoencoders

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -13,8 +13,9 @@ def autoencoder(input_dims, filters, latent_dims):
     encoder_inputs = keras.Input(shape=input_dims)
     x = encoder_inputs
     for f in filters:
-        x = keras.layers.Conv2D(f, (3, 3), activation='relu',
-                               padding='same')(x)
+        x = keras.layers.Conv2D(
+            f, (3, 3), activation='relu', padding='same'
+        )(x)
         x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
 
     encoder = keras.Model(encoder_inputs, x, name='encoder')
@@ -23,18 +24,26 @@ def autoencoder(input_dims, filters, latent_dims):
     decoder_inputs = keras.Input(shape=latent_dims)
     x = decoder_inputs
 
-    for i in range(len(filters) - 1):
-        x = keras.layers.Conv2D(filters[::-1][i + 1], (3, 3),
-                               activation='relu', padding='same')(x)
+    # Use the correct slice of reversed filters: [8, 16]
+    dec_filters = filters[::-1][1:]
+
+    for f in dec_filters:
+        x = keras.layers.Conv2D(
+            f, (3, 3), activation='relu', padding='same'
+        )(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
 
-    x = keras.layers.Conv2D(filters[0], (3, 3), activation='relu',
-                           padding='valid')(x)
+    # Second to last layer uses 'valid' padding
+    x = keras.layers.Conv2D(
+        filters[0], (3, 3), activation='relu', padding='valid'
+    )(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
 
+    # Final convolution layer matching original image channel dimension
     channels = input_dims[-1]
-    outputs = keras.layers.Conv2D(channels, (3, 3), activation='sigmoid',
-                                 padding='same')(x)
+    outputs = keras.layers.Conv2D(
+        channels, (3, 3), activation='sigmoid', padding='same'
+    )(x)
 
     decoder = keras.Model(decoder_inputs, outputs, name='decoder')
 
