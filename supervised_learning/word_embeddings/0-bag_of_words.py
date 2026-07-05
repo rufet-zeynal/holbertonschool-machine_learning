9ec8ef4263fed20b56373b51bddd93a66d08d246
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-07-05 12:25:43 +0400

    NLP

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""BoW module"""
+import numpy as np
+import re
+
+
+def bag_of_words(sentences, vocab=None):
+    """Creates BoW matrix"""
+    if vocab is None:
+        # Get unique words >= 2 chars
+        vocab = sorted(set(w for s in sentences for w in re.findall(r'\b\w\w+\b', s.lower())))
+
+    # Init matrix
+    E = np.zeros((len(sentences), len(vocab)), dtype=int)
+
+    # Count words
+    for i, s in enumerate(sentences):
+        for w in re.findall(r'\b\w+\b', s.lower()):
+            if w in vocab:
+                E[i, vocab.index(w)] += 1
+
+    return E, np.array(vocab)
