17284743da50d50ab7af71f5a1ec27db883cfee5
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-07-05 12:41:21 +0400

    NLP

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -5,15 +5,28 @@ import gensim
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates and trains a Word2Vec model"""
-    return gensim.models.Word2Vec(
-        sentences=sentences,
+    """Creates, builds and trains a gensim word2vec model"""
+    sg = 0 if cbow else 1
+
+    # 1. Create the model
+    model = gensim.models.Word2Vec(
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=0 if cbow else 1,
-        epochs=epochs,
+        sg=sg,
         seed=seed,
         workers=workers
     )
+
+    # 2. Build the vocabulary
+    model.build_vocab(sentences)
+
+    # 3. Train the model
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
+
+    return model
