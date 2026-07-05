548bbb434cfd9b58b26cebe17c08d0e424ddb3ee
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-07-05 12:40:04 +0400

    NLP

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -5,18 +5,14 @@ import gensim
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates, builds and trains a gensim word2vec model"""
-    # Determine training type: sg=0 for CBOW, sg=1 for Skip-gram
-    sg = 0 if cbow else 1
-
-    # Passing sentences here automatically builds vocab and trains the model
+    """Creates and trains a Word2Vec model"""
     return gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=sg,
+        sg=0 if cbow else 1,
         epochs=epochs,
         seed=seed,
         workers=workers
