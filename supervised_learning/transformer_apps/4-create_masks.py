58d7af6a49dee7a59133700fa30a691a2b76cd49
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-07-19 17:07:53 +0400

    Transformers

--- supervised_learning/transformer_apps/4-create_masks.py
+++ supervised_learning/transformer_apps/4-create_masks.py
@@ -9,15 +9,12 @@ def create_masks(inputs, target):
     """
     Creates all masks for training/validation
     """
-
     def padding_mask(seq):
         mask = tf.cast(tf.math.equal(seq, 0), tf.float32)
         return mask[:, tf.newaxis, tf.newaxis, :]
 
     encoder_mask = padding_mask(inputs)
     decoder_mask = padding_mask(inputs)
-
-
     seq_len_out = tf.shape(target)[1]
     look_ahead_mask = 1 - tf.linalg.band_part(
         tf.ones((seq_len_out, seq_len_out)), -1, 0)
