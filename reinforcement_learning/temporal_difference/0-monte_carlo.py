5fa80430957a28f2d391cf5b5e53bf2090d8d465
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-08-29 15:44:54 +0400

    Reinforcement Learning

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -13,12 +13,12 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
 
         for _ in range(max_steps):
             action = policy(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
+            next_state, reward, done, truncated, _ = env.step(action)
 
             episode.append((state, reward))
             state = next_state
 
-            if terminated or truncated:
+            if done:
                 break
 
         G = 0
