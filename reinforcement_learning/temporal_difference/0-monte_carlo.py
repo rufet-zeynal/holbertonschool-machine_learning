88634e90f33c9a951ecaf52d2bc86a349fb53a4e
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-08-29 21:12:57 +0400

    Reinforcement Learning

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -5,8 +5,7 @@ import numpy as np
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """
-    Performs the Monte Carlo algorithm for value estimation.
+    """Performs the Monte Carlo algorithm for value estimation.
 
     Parameters:
         env: environment instance
