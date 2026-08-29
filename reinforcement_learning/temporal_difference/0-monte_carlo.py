60df15ee0341e12f337aef47e469aea1518a6293
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-08-29 15:41:50 +0400

    Reinforcement Learning

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Monte Carlo algorithm."""
 
+import numpy as np
+
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """Perform the Monte Carlo algorithm."""
+    """Performs the Monte Carlo algorithm."""
     for _ in range(episodes):
         state, _ = env.reset()
         episode = []
@@ -12,6 +14,7 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
         for _ in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
+
             episode.append((state, reward))
             state = next_state
 
